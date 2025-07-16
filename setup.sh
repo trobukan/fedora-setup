d6bcf3d8891f2c143f91e3ab6e002109c5f6720d
trobukan <trobukan@gmail.com>
2025-07-16 16:05:28 -0300

    update DNF's cache

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ sudo dnf update -y
 sudo dnf install -y \
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+sudo dnf install make cache
