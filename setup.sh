ef63c140adbaa5f04747af6ccd6a4fb67753da18
trobukan <trobukan@gmail.com>
2025-07-16 16:04:04 -0300

    download rpm repository

---
+++ setup.sh
@@ -0,0 +1,6 @@
+set -e
+sudo dnf update -y
+
+sudo dnf install -y \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
