9fdb348c7ee71d49bbcf6d9034be160df07250cd
trobukan <trobukan@gmail.com>
2025-07-16 16:20:18 -0300

    install steam htop

--- setup.sh
+++ setup.sh
@@ -5,7 +5,12 @@ sudo dnf install -y \
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-sudo dnf install makecache
+sudo dnf install -y makecache
 sudo dnf install -y akmod-nvidia
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub app.zen_browser.zen
+flatpak install -y flathub com.spotify.Client
+flatpak install -y flathub md.obsidian.Obsidian
+
+sudo dnf install -y steam htop 
