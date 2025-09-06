f8d8801f785f177c4ae34c8cc43ae8fae4f2d17c
trobukan <trobukan@gmail.com>
2025-09-06 15:18:42 -0300

    [feat] Remove unnecessary packages

--- setup.sh
+++ setup.sh
@@ -12,16 +12,8 @@ sudo dnf install -y akmod-nvidia
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub app.zen_browser.zen
-flatpak install -y flathub com.spotify.Client
-flatpak install -y flathub md.obsidian.Obsidian
 
-sudo dnf install -y steam htop fastfetch jetbrains-mono-fonts nodejs npm docker docker-compose golang nvim kitty
-
-git config --global user.name = "trobukan"
-git config --global user.email = "trobukan@gmail.com"
-git config --global init.defaultBranch main
-git config --global color.ui auto
-git config --global pull.rebase false
-git config --global core.editor "nvim"
+sudo dnf install -y htop fastfetch jetbrains-mono-fonts nodejs npm docker docker-compose golang nvim kitty stow
 
 plasma-apply-wallpaperimage ./wallpapers/wallpaper.jpg
+
